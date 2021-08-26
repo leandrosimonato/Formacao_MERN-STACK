3a79d5310c7954a896357d4e8afd21f758cca9ec
leandrosimonato <leandrosimonato@gmail.com>
2021-08-26 16:46:08 -0300

    Criando um relógio.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from 'react';
-
+import './App.css'
 
 
 function App() {
-
-  // Declare uma nova variável de state, a qual chamaremos de "count"
-
-  const [count, setCount] = useState(0);
-  const [nome, setNome] = useState("Leandro");
   
-  function callAlert() {
-
-  }
+  const [hora, setHora] = useState(4)
+  const [minuto, setMinuto] = useState(59)
+  const [segundo, setSegundo] = useState(59)
 
   useEffect(() => {
-    callAlert();
-  }, [count])
+    const interval = setInterval(() => {
+      setSegundo(segundo+1)
+      if (segundo === 59) {
+        setSegundo(0)
+        setMinuto(minuto+1)
+        if(minuto === 59) {
+          setMinuto(0)
+          setHora(hora+1)
+        }
+      }
+    }, 1000)
+    return() => clearInterval(interval)
+  })
 
     return (
       <div>
-        {
-          (nome == "Leandro")?
-            <div>
-              <p>{nome}</p>
-            </div>:
-            <div>
-              <p>Opss eu preciso do nome Leandro.</p>
-            </div>
-        }
+        <h2>
+          {hora}:{minuto}:{segundo}
+        </h2>
       </div>
     );
 }
